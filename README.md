f623ce1058c31252577f88391fd9caf9331a42c9
zxffffffff <34299738+zxffffffff@users.noreply.github.com>
2023-10-09 13:38:15 +0800

    增加antd

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # start-web-react
  一个 Web React 脚手架项目，使用 TypeScript 开发
 
-## 安装
-- 需要安装 Node.js 用于本地开发，生产环境可以导出 HTML/CSS/JS 等静态文件
+## react
+- Facebook（Meta），MIT 开源，Star 200+K
+- 用于构建 GUI 的 JavaScript 库。
+
+### 安装
+- 需要安装 Node.js 用于本地开发
 - 参考：https://github.com/facebook/create-react-app
 ```bash
 # 可选：淘宝镜像源
@@ -11,15 +15,29 @@ npm config set registry https://registry.npm.taobao.org
 npm uninstall -g create-react-app
 npm install -g create-react-app
 ```
-- 创建项目
+
+### 创建工程
 ```bash
 npx create-react-app start-web-react --template typescript
 cd start-web-react
 npm start
 ```
-- 管理项目
+
+### 运行
 ```bash
 npm start     # 开发模式
 npm test      # 单元测试
 npm run build # 打包生产（build 文件夹）
 ```
+
+## antd
+- 蚂蚁集团（阿里），MIT 开源，Star 80+K
+- 基于 Ant Design 设计体系的 React UI 组件库，主要用于研发企业级中后台产品。
+
+### 安装
+```bash
+npm install antd --save
+```
+
+### 使用组件
+- 参考：https://ant-design.antgroup.com/components/overview-cn/
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { DatePicker } from 'antd';
 import Game from './TicTacToe';
 import FilterableProductTable from './FilterableProductTable';
 
@@ -8,7 +9,6 @@ import FilterableProductTable from './FilterableProductTable';
 function MyButton() {
   // 以 use 开头的函数称为 Hooks 钩子。
   const [count, setCount] = useState(0); // setCount 触发更新屏幕
-
   const ref = useRef(0);                 // 仅引用，不触发更新屏幕
 
   // 处理渲染无关的操作（网络、IO、定时器等）
@@ -16,9 +16,8 @@ function MyButton() {
   useEffect(() => {
     // 监听 props 或 state 的变化，执行操作
     const timer = setInterval(() => { /* task */ }, 1000);
-
+    // 清理操作
     return () => {
-      // 清理操作
       clearInterval(timer);
     };
   }, [ref]);
@@ -30,8 +29,8 @@ function MyButton() {
   }
 
   // 显示数据：JSX 允许您将标记放入 JavaScript 中，例如
-  //          1、<h1> {user.name} </h1>
-  //          2、<img className="avatar" src={user.imageUrl} />
+  // 1、<h1> {user.name} </h1>
+  // 2、<img className="avatar" src={user.imageUrl} />
   return (
     <button onClick={handleClick}>
       Clicked {count} times
@@ -40,8 +39,8 @@ function MyButton() {
 }
 
 // 添加样式：可以使用 className，它的工作方式与 HTML class 属性相同，
-//          1、<header style={ border: '2px solid orange', color: 'black', fontSize: '18px' }>
-//          2、在单独的 CSS 文件中为其编写 className.{ xxx }
+// 1、<header style={ border: '2px solid orange', color: 'black', fontSize: '18px' }>
+// 2、在单独的 CSS 文件中为其编写 className.{ xxx }
 function App() {
   return (
     <div className="App">
@@ -59,6 +58,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <h1>
+        <DatePicker />
+      </h1>
       <h1>
         <MyButton />
         <MyButton />
